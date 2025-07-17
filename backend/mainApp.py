d8ec3e1220005af123d4a2b9a461dfea43d9321e
Shayaan Rahim <shayaan5060@gmail.com>
2025-07-17 13:54:47 -0500

    stage

--- backend/mainApp.py
+++ backend/mainApp.py
@@ -1,1 +1,24 @@
 #Main Flask app
+
+# backend/app.py
+from flask import Flask, jsonify
+app = Flask(__name__)
+
+@app.route('/api/news')
+def get_news():
+    return "Geopolitical News"
+
+@app.route('/api/impact')
+def analyze_impact():
+    return "Geopolitical Impact"
+
+@app.route('/api/historical')
+def get_historical():
+    return "Get Historical"
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
